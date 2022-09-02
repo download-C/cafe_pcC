edb4884327f05df4be785d1ad0467a6e06f0d660
Choi Daun <ekdnsl13@naver.com>
2022-09-03 02:12:41 +0900

    [add] 테이블이름으로 db, action 패키지 생성
    	com.pcc.테이블명.db
    	com.pcc.테이블명.action
    ------------------------------------------
    [add] db 패키지에 DTO, DAO 클래스 생성
    
    	DTO : 변수 선언, 게터/세터, toString()
    	DAO : getConnect(), closeDB() 
    	
    ------------------------------------------
    
    [add] Action 인터페이스와 ActionForward 클래스 생성
    
    	ActionForward: execute()
    	
    ------------------------------------------
    
    [add] 각 테이블의 action 패키지에 FrontController 서블릿 생성 
    
    	1. 가상 주소 계산 
    	2. 가상 주소 매핑 양식
    	3. 가상 주소 이동 
    	
    ------------------------------------------
    
    [add] web.xml 파일에 각 테이블별 매핑 주소 생성
    	1. board -> *.bo 
    	2. cart -> *.ca
    	3. member -> *.me
    	4. order -> *.or
    	5. product -> *.pr
    	6. reservation -> *.re
    ------------------------------------------
    
    [add] WebContent 폴더에 index.jsp 생성

---
+++ src/com/pcc/member/db/MemberDAO.java
@@ -0,0 +1,107 @@
+package com.pcc.member.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+// 회원 관련 모든 메서드를 생성하는 클래스
+
+public class MemberDAO {
+
+	private Connection con = null;
+	private String sql ="";
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	public MemberDAO () {
+		System.out.println("DAO : DB 연결을 위한 모든 정보 준비 완료");
+	}
+	
+	// 1. CP를 이용한 DB 연결 -----------------------------------------
+	private Connection getConnect() {
+		try {
+			// 1-1. 프로젝트 정보 초기화
+			Context initCTX = new InitialContext();
+			// 1-2. 초기화된 프로젝트 중 데이터 관련 정보 불러오기
+			DataSource ds = (DataSource) initCTX.lookup("java:comp/env/jdbc/cafe_pcc");
+			con = ds.getConnection();
+			
+			System.out.println("DAO : DB 연결 완료");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
+	
+	// 2. getConnect() 메서드의 역순으로 DB 연결 해제 ------------------
+	
+	private void closeDB() {
+		try {
+			if(rs != null)	rs.close();
+			if(pstmt != null)  pstmt.close();
+			if(con != null)  con.close();
+			System.out.println("DAO : DB 자원(rs, pstmt, con) 해제 완료");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("DAO : DB 연결 해제");
+	}
+	
+	
+	// 3.   -----------------------------------------
+	
+	
+	
+	
+	// 4.   -----------------------------------------
+	
+	
+	
+	
+	// 5.   -----------------------------------------
+	
+	
+	
+	
+	// 6.   -----------------------------------------
+	
+	
+	
+	
+	// 7.   -----------------------------------------
+	
+	
+	
+	
+	// 8.   -----------------------------------------
+	
+	
+	
+	
+	// 9.   -----------------------------------------
+	
+	
+	
+	
+	// 10.   -----------------------------------------
+	
+	
+	
+	
+	
+	
+	
+}
+
